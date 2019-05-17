b62cf1e7631f9b72c1cb11af8d4ccbc0a9ec0b60
Frantz Darbon <frantz.darbon@telecomnancy.eu>
2019-05-17 23:59:18 +0200

    Ça avance

--- src/games/phoenix/Player.java
+++ src/games/phoenix/Player.java
@@ -2,6 +2,10 @@ package games.phoenix;
 
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
+import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.geom.Ellipse;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
 
 import app.AppPlayer;
 
@@ -17,7 +21,12 @@ public class Player {
 	private int speed;
 	private int height;
 	private int width;
+	private SpriteSheet playerSpritSheet;
 	private Animation animation[];
+	private Ellipse headHitbox;
+	private Rectangle bodyHitbox;
+	private Shape hitbox;
+
 
 	/**
 	 * Constructeur du joueur
@@ -26,12 +35,26 @@ public class Player {
 	public Player (AppPlayer appPlayer, World world) {
 		this.controllerID = appPlayer.getControllerID ();
 		this.name = appPlayer.getName ();
-		this.posX = world.getHeight();
-		this.posY = world.getWidth();
 		this.speed = 1;
 		this.height = 80;
 		this.width = 80;
-		this.animation = new Animation [4]; 
+		this.posX = (world.getHeight() - height)/2;
+		this.posY = (world.getWidth() - width)/2;
+		this.playerSpritSheet = new SpriteSheet(image, tw, th);
+		this.animation = new Animation [4];
+
+		try {
+			animation [0] = loadAnimation(playerSpritSheet, 0, 1, 0);
+			animation [1] = loadAnimation(playerSpritSheet, 0, 1, 1);
+			animation [2] = loadAnimation(playerSpritSheet, 0, 1, 2);
+			animation [3] = loadAnimation(playerSpritSheet, 0, 1, 3);
+		} catch (SlickException e) {
+			e.printStackTrace ();
+		}
+
+		this.headHitbox = new Ellipse(posX + 40, posY + 26, 26, 24);
+		this.bodyHitbox = new Rectangle(posX + 30, posY + 46, 22, 32);
+		this.hitbox = headHitbox.union(bodyHitbox)[0];
 	}
 
 	/**
