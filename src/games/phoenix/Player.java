736e80893b0ec1e01083a5cd5ae4ce3ee05813a8
Frantz Darbon <frantz.darbon@telecomnancy.eu>
2019-05-19 00:50:18 +0200

    Les accesseurs

--- src/games/phoenix/Player.java
+++ src/games/phoenix/Player.java
@@ -83,7 +83,7 @@ public class Player {
 		AppInput input = (AppInput) container.getInput ();
 		
 		move(input, delta);
-		
+
 	}
 
 	/**
@@ -201,5 +201,21 @@ public class Player {
 	{
 		float[] pos = {posX,posY};
 		return pos;
+	}
+
+	public int getHeight(){
+		return this.height;
+	}
+
+	public void setHeight(int height){
+		this.height = height;
+	}
+
+	public int getWidth(){
+		return this.width;
+	}
+
+	public void setWidth(int width){
+		this.width = width;
 	}
 }
