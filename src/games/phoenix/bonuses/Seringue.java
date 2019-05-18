904206143c4396be95b0935f06ff755a3d1a0b87
bruno <bruno.gomes-dos-santos@telecomnancy.eu>
2019-05-18 23:53:44 +0200

    Mise en route de Bruno. Et classe Bonus + package bonuses avec Seringue fait

---
+++ src/games/phoenix/bonuses/Seringue.java
@@ -0,0 +1,49 @@
+package games.phoenix.bonuses;
+
+import app.AppLoader;
+import games.phoenix.Bonus;
+import games.phoenix.Player;
+
+import games.phoenix.World;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Seringue extends Bonus {
+
+    private boolean activated, deleted;
+
+    static{
+        sprite = AppLoader.loadPicture(World.IMAGES + "Chemin à depuis phoenix");
+    }
+
+    public Seringue(int caseX, int caseY){
+        super (caseX, caseY);
+        this.activated = false;
+        this.deleted = false;
+    }
+
+    public void activate (Player player) {
+        if (!isActivated ()) {
+            this.activated = true;
+            player.addLife (1);
+            this.deleted = true;
+        }
+    }
+
+    public void update (GameContainer container, StateBasedGame game, int delta) {}
+
+    public void render (GameContainer container, StateBasedGame game, Graphics context) {
+        super.render (container, game, context);
+    }
+
+    public boolean isActivated () {
+        return this.activated;
+    }
+
+    public boolean isDeleted () {
+        return this.deleted;
+    }
+
+}
