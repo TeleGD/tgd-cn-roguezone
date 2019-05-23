5cf1311ca9ca13474d48735300a0c527cc536d39
Quentin Charrier <quentin.charrier@telecomnancy.eu>
2019-05-23 15:39:11 +0200

    correction de la structure des ennemi + ajout d'un nouveau mouvement

--- src/games/phoenix/enemies/EnemyBoss.java
+++ src/games/phoenix/enemies/EnemyBoss.java
@@ -1,13 +1,23 @@
 package games.phoenix.enemies;
 
+import org.newdawn.slick.geom.Ellipse;
+import org.newdawn.slick.geom.Rectangle;
+
 import games.phoenix.Player;
 
 public class EnemyBoss extends Enemy{
 	
 	public EnemyBoss(int x, int y, Player player)
 	{
-		super(0, "boss", x, y, EnemyColor.BOSS);
+		super(0, "boss", x, y);
 		setBehavior(new EnemyBehavior(this, player));
+		this.image = playerSpriteSheet.get("Boss");
+		setContactDamage(5);
+		speed = 2;
+		pv = 3;
+		this.headHitbox = new Ellipse(posX + 48, posY + 32, 32, 30);
+		this.bodyHitbox = new Rectangle(posX + 36, posY + 55, 26, 38);
+		this.hitbox = headHitbox.union(bodyHitbox)[0];
 		getBehavior().setPlayerInfluence(EnemyBehavior.interacting.COMING);
 	}
 }
--- src/games/phoenix/enemies/EnemyGreen.java
+++ src/games/phoenix/enemies/EnemyGreen.java
@@ -1,13 +1,20 @@
 package games.phoenix.enemies;
 
+import org.newdawn.slick.geom.Ellipse;
+
 import games.phoenix.Player;
 
 public class EnemyGreen extends Enemy{
 	
 	public EnemyGreen(int x, int y, Player player)
 	{
-		super(0, "green", x, y, EnemyColor.GREEN);
-		setBehavior(new EnemyBehavior(this, player));
+		super(0, "green", x, y);
+		this.image = playerSpriteSheet.get("Vert");
+		setContactDamage(2);
+		this.speed = (float)1.2;
+		this.pv = 3;
+		this.hitbox = new Ellipse(posX, posY, size/2, size/2);
 		getBehavior().setPlayerInfluence(EnemyBehavior.interacting.COMING);
+		setBehavior(new EnemyBehavior(this, player));
 	}
 }
